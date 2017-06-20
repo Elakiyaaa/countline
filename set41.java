f735087dd6bf87a3cb74a086bf6c4cfc9dbfa5ad
Elakiyaaa <elakthamizh@gmail.com>
2017-06-20 09:28:42 +0530

    Add files via upload

---
+++ set41.java
@@ -0,0 +1,15 @@
+package begiiners;
+
+import java.util.Scanner;
+
+public class set41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a=s.next();
+int n=a.length();
+System.out.print(n);
+	}
+
+}
